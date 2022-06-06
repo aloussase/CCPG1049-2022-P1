a259e1e78dd8c5ab32dc332b53c7ae034011b16f
anntnzrb <anntnzrb@protonmail.com>
2022-06-06 18:02:17 -0500

    C: init base file

---
+++ proj01.c
@@ -0,0 +1,18 @@
+/* proj01.c --- Archivo principal del programa en C */
+
+/**
+ * \file        proj01.c
+ * \brief       Archivo principal del programa en C
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int
+main(int argc, char **argv)
+{
+    /* int foo = bar(); */
+
+    return 0;
+}
