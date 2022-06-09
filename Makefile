4971b817ec0cb2a6f1041608d3a9d9518969d64f
Alexander Goussas <goussasalexander@gmail.com>
2022-06-09 17:37:16 -0500

    implemented C version of the project

--- Makefile
+++ Makefile
@@ -1,38 +1,24 @@
 # C
-CC     := cc
-CFLAGS := -std=c99 -pedantic -Wall -Wextra -Wno-deprecated-declarations -Os
+CC     := gcc
+CFLAGS := -std=c11 -pedantic -Wall -Wextra -Wno-deprecated-declarations -Os
+LDFLAGS := -lreadline
 
 # MIPS
 MIPS := spim -file
 
-NAME = proj01-x
-SRC  = proj01.c
+SRC  = main.c
 OBJ  = ${SRC:.c=.o}
 
-all: options
-
-options:
-	@printf "=> Opciones de compilador C:\n"
-	@printf "CC     = ${CC}\n"
-	@printf "CFLAGS = ${CFLAGS}\n"
-
-	@printf "\n=> Opciones de compilador MIPS:\n"
-	@printf "MIPS     = ${MIPS}\n"
-
-	@printf "\n=> Opciones:\n"
-	@printf "make c     --- Compila y genera el archivo .c\n"
-	@printf "make mips  --- Compila y corre el archivo .asm\n"
-	@printf "make asm   --- alias de 'make mips'\n"
-	@printf "make clean --- Elimina objetos y otra basura\n"
+all: main
 
 clean:
-	rm -f ${NAME:-x=-c} *.o
+	rm -f main *.o
 
 # -----------------------------------------------------------------------------
 # MIPS (ASM) Compilation
 # -----------------------------------------------------------------------------
 
-mips: proj01.asm
+mips: main.asm
 	${MIPS} ${^}
 
 asm: mips
@@ -44,8 +30,7 @@ asm: mips
 .c.o:
 	${CC} -c ${CFLAGS} ${<}
 
-c: ${OBJ}
-	${CC} -o ${NAME:-x=-c} ${OBJ}
-	@printf "\n\nExecutable generado, correr con: ./%s\n" ${NAME:-x=-c}
+main: ${OBJ}
+	${CC} -o $@ $^ $(LDFLAGS)
 
 .PHONY: all clean options c mips asm
---
+++ main.c
@@ -0,0 +1,135 @@
+#include <assert.h>
+#include <readline/readline.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define EQLFLT(x, y) (((x) - (y)) < 0.00001)
+
+int
+is_valid_coin(double coin)
+{
+  return EQLFLT(coin, 0.05) | EQLFLT(coin, 0.10) | EQLFLT(coin, 0.25) | EQLFLT(coin, 0.5);
+}
+
+float
+ask_for_balance()
+{
+  double coin;
+  double saldo = 0;
+
+  char* line;
+  while ((line = readline("Ingrese monedas: ")) != NULL)
+    {
+      coin = atof(line);
+
+      if (coin == -1.0)
+        {
+          free(line);
+          break;
+        }
+
+      if (!is_valid_coin(coin))
+        {
+          fprintf(stderr, "Moneda incorrecta\n");
+        }
+      else
+        {
+          saldo += coin;
+        }
+
+      free(line);
+    }
+
+  return saldo;
+}
+
+int
+is_valid_phone_number(char* number)
+{
+  if (strlen(number) != 10)
+    return 0;
+
+  while (*number)
+    {
+      if (!(*number >= '0' && *number <= '9'))
+        return 0;
+      number++;
+    }
+
+  return 1;
+}
+
+void
+ask_for_phone_number()
+{
+  char* line;
+
+  while ((line = readline("Ingrese el numero a llamar: ")) != NULL)
+    {
+      if (is_valid_phone_number(line))
+        {
+          free(line);
+          return;
+        }
+
+      fprintf(stderr, "Numero incorrecto\n");
+      free(line);
+    }
+}
+
+int
+simulate_call()
+{
+  int minutes = 0;
+  char* ans = readline("Iniciar la llamada? ");
+
+  if (strcmp(ans, "Si") != 0)
+    return minutes;
+
+  free(ans);
+
+  while (1)
+    {
+      minutes += 1;
+      printf("%d. Llamada en curso ... Presiona C para colgar\n", minutes);
+
+      int c = getchar();
+      if (c == 'C')
+        break;
+    }
+
+  return minutes;
+}
+
+int
+run()
+{
+
+  float saldo = ask_for_balance();
+  printf("Su saldo es %.2f\n", saldo);
+  printf("\n");
+
+  int price_per_minute = (rand() + 10) % 40;
+  printf("Costo de la llamada: $ 0.%d\n", price_per_minute);
+
+  ask_for_phone_number();
+  int minutes = simulate_call();
+
+  float final_price = (price_per_minute / 100.0) * minutes;
+  printf("Costo de la llamada: $ %.2f\n", final_price);
+  // TODO: Duracion de la llamada
+
+  printf("Cambio: $ %.2f\n", saldo - final_price);
+
+  return 0;
+}
+
+int
+main()
+{
+  srand(time(NULL));
+  return run();
+}
