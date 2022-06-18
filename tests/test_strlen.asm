6a9cdae5793af710de73e79950243e48b9eb5073
anntnzrb <anntnzrb@protonmail.com>
2022-06-18 12:50:01 -0500

    tests/: minor formatting

--- tests/test_strlen.asm
+++ tests/test_strlen.asm
@@ -1,9 +1,9 @@
         .data
-small_string:   .asciiz "Hello, World!"
-long_string:    .asciiz "Everything, everywhere, all at once"
+small_string: .asciiz "Hello, World!"
+long_string:  .asciiz "Everything, everywhere, all at once"
 
-small_string_test_failed:  .asciiz "\033[31mERROR:\033[m small_string test failed\n"
-long_string_test_failed:        .asciiz "\033[31mERROR:\033[m long_string test failed\n"
+small_string_test_failed: .asciiz "\033[31mERROR:\033[m small_string test failed\n"
+long_string_test_failed:  .asciiz "\033[31mERROR:\033[m long_string test failed\n"
 
         .text
         .globl main
@@ -14,29 +14,32 @@ main:
         addi $sp, $sp, -4
         sw   $ra, 0($sp)
 
-        # Test 1
+        # Test 1 ("Hello, World!" == 13)
 
-        la $a0, small_string
+        la  $a0, small_string
         jal strlen
 
         li $t0, 13 # Expected length
 
-        la $a0, small_string_test_failed
+        la  $a0, small_string_test_failed
         bne $v0, $t0, EXIT_ERROR
 
-        # Test 2
+        # Test 2 ("Everything, everywhere, all at once" == 35)
 
-        la $a0, long_string
+        la  $a0, long_string
         jal strlen
 
         li $t0, 35 # Expected length
 
-        la $a0, long_string_test_failed
+        la  $a0, long_string_test_failed
         bne $v0, $t0, EXIT_ERROR
 
         lw   $ra, 0($sp)
         addi $sp, $sp, 4
+
         jr $ra
+
+        # TODO :: end main
 
 EXIT_ERROR:
         li $v0, 4 # Print the error message
@@ -44,4 +47,5 @@ EXIT_ERROR:
 
         lw   $ra, 0($sp)
         addi $sp, $sp, 4
+
         jr $ra
--- tests/tests_is_valid_phone_number.asm
+++ tests/tests_is_valid_phone_number.asm
@@ -1,11 +1,11 @@
         .data
-valid_phone_number:     .asciiz "0995764993"
-invalid_phone_number:   .asciiz "12345678910"
-string_not_a_number:    .asciiz "Hello, World!"
+valid_phone_number:   .asciiz "0995764993"
+invalid_phone_number: .asciiz "12345678910"
+string_not_a_number:  .asciiz "Hello, World!"
 
-valid_phone_number_error:       .asciiz "\033[31mERROR:\033[m valid_phone_number returned false"
-invalid_phone_number_error:       .asciiz "\033[31mERROR:\033[m invalid_phone_number returned true"
-string_not_a_number_error:      .asciiz "\033[31mERROR:\033[m string_not_a_number returned true"
+valid_phone_number_error:   .asciiz "\033[31mERROR:\033[m valid_phone_number returned false"
+invalid_phone_number_error: .asciiz "\033[31mERROR:\033[m invalid_phone_number returned true"
+string_not_a_number_error:  .asciiz "\033[31mERROR:\033[m string_not_a_number returned true"
 
         .text
         .globl main
@@ -17,27 +17,30 @@ main:
         addi $sp, $sp, -4
         sw   $ra, 0($sp)
 
-        # TEST 1
+        # Test 1 ("0995764993")
         la   $a0, valid_phone_number
         jal  is_valid_phone_number
         la   $a0, valid_phone_number_error
         beqz $v0, EXIT_ERROR
 
-        # TEST 2
-        la   $a0, invalid_phone_number
-        jal  is_valid_phone_number
-        la   $a0, invalid_phone_number_error
-        bne  $v0, $zero, EXIT_ERROR
+        # Test 2 ("12345678910")
+        la  $a0, invalid_phone_number
+        jal is_valid_phone_number
+        la  $a0, invalid_phone_number_error
+        bne $v0, $zero, EXIT_ERROR
 
-        # TEST 3
-        la   $a0, string_not_a_number
-        jal  is_valid_phone_number
-        la   $a0, string_not_a_number_error
-        bne  $v0, $zero, EXIT_ERROR
+        # Test 3 ("Hello, World!")
+        la  $a0, string_not_a_number
+        jal is_valid_phone_number
+        la  $a0, string_not_a_number_error
+        bne $v0, $zero, EXIT_ERROR
 
         lw   $ra, 0($sp)
         addi $sp, $sp, 4
-        jr   $ra
+
+        jr $ra
+
+        # TODO :: end main
 
 EXIT_ERROR:
         li $v0, 4
@@ -45,4 +48,5 @@ EXIT_ERROR:
 
         lw   $ra, 0($sp)
         addi $sp, $sp, 4
-        jr   $ra
+
+        jr $ra
