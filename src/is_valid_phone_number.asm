c9cd7a13515d75254abb56614efb8dcf8b782aab
Alexander Goussas <goussasalexander@gmail.com>
2022-06-20 11:58:03 -0500

    Fixed error when not saving argument registers
    In is_valid_phone_number we were calling strlen without saving the
    $a0 register. strlen increments this pointer while iterating, so
    when it returns we were left with $a0 pointing to the null byte.
    
    Callers should save argument registers before making a function call.

--- src/is_valid_phone_number.asm
+++ src/is_valid_phone_number.asm
@@ -5,11 +5,14 @@
         # Output:
         #   $v0: 1 if the string is a valid phone number, 0 otherwise.
 is_valid_phone_number:
-        addi $sp, $sp, -4
+        addi $sp, $sp, -8
         sw   $ra, 0($sp)                             # Save return address in stack
+        sw   $s0, 4($sp)
 
+        move $s0, $a0                                # Save $a0
         jal  strlen                                  # Calculate the length of the input string.
         move $t1, $v0                                # Save return value of strlen
+        move $a0, $s0                                # Restore $a0
 
         li   $v0, 0
         li   $t0, 10
@@ -24,15 +27,17 @@ is_valid_phone_number_loop:
         li   $v0, 0                                 # Prepare false return value in case any of the following are true
 
         li   $t1, 48                                # 48 is ascii code for 0
-        blt  $t0, $t1, is_valid_phone_number_exit   # *$a0 < 0
+        blt  $t0, $t1, is_valid_phone_number_exit   # *$a0 < '0'
 
         li   $t1, 57                                # 57 is 9
-        bgt  $t0, $t1, is_valid_phone_number_exit   # *$a0 > 9
+        bgt  $t0, $t1, is_valid_phone_number_exit   # *$a0 > '9'
 
         addi $a0, $a0, 1                            # Increment the string pointer
         j    is_valid_phone_number_loop
 
 is_valid_phone_number_exit:
         lw   $ra, 0($sp)
-        addi $sp, $sp, 4
+        lw   $s0, 4($sp)
+        addi $sp, $sp, 8
+
         jr   $ra
--- src/strlen.asm
+++ src/strlen.asm
@@ -5,12 +5,15 @@
         # Output:
         #   $v0: The length of the given string.
 strlen:
-        li          $v0, 0
-strlen1:
+        li   $v0, 0
+
+strlen_loop:
         lb          $t0, 0($a0)
-        beqz        $t0, strlen2
+        beqz        $t0, strlen_exit
+
         addi        $v0, $v0, 1
         addi        $a0, $a0, 1
-        j           strlen1
-strlen2:
+        j           strlen_loop
+
+strlen_exit:
         jr          $ra
